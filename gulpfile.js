0e42bb9440dbfd5d368e0ef8adbf2fcc09f69f25
Cerasus04 <cerasus1004@gmail.com>
2021-04-15 21:41:53 +0300

    Сетки начало

--- gulpfile.js
+++ gulpfile.js
@@ -85,7 +85,7 @@ gulp.task("html", function () {
 gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
+    "source/img/**.{webp,svg}",
     "source/js/**",
     "source//*.ico"
     ], {
